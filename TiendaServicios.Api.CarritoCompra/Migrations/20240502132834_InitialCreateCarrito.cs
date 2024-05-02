df68e68df7a2557dae52a6163ecd144ef78837e7
facundomarcos <facundomarcos@live.com.ar>
2024-05-02 10:49:36 -0300

    carrito compra

---
+++ TiendaServicios.Api.CarritoCompra/Migrations/20240502132834_InitialCreateCarrito.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaServicios.Api.CarritoCompra.Migrations
+{
+    public partial class InitialCreateCarrito : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarritoSesion",
+                columns: table => new
+                {
+                    CarritoSesionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarritoSesion", x => x.CarritoSesionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarritoSesionDetalle",
+                columns: table => new
+                {
+                    CarritoSesionDetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ProductoSeleccionado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CarritoSesionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarritoSesionDetalle", x => x.CarritoSesionDetalleId);
+                    table.ForeignKey(
+                        name: "FK_CarritoSesionDetalle_CarritoSesion_CarritoSesionId",
+                        column: x => x.CarritoSesionId,
+                        principalTable: "CarritoSesion",
+                        principalColumn: "CarritoSesionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarritoSesionDetalle_CarritoSesionId",
+                table: "CarritoSesionDetalle",
+                column: "CarritoSesionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarritoSesionDetalle");
+
+            migrationBuilder.DropTable(
+                name: "CarritoSesion");
+        }
+    }
+}
---
+++ TiendaServicios.Api.CarritoCompra/Program.cs
@@ -0,0 +1,36 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TiendaServicios.Api.CarritoCompra.Aplicacion;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("ConexionDatabaseCarrito");
+builder.Services.AddDbContext<CarritoContexto>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
+//builder.Services.AddAutoMapper(typeof(Consulta.Manejador));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

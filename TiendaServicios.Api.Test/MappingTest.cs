b9de295bb948e3090dd1e38b5d83339be02020a9
facundomarcos <facundomarcos@live.com.ar>
2024-05-07 09:16:37 -0300

    1er test unitario exitoso

---
+++ TiendaServicios.Api.Test/MappingTest.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Libro.Aplicacion;
+using TiendaServicios.Api.Libro.Modelo;
+
+namespace TiendaServicios.Api.Test
+{
+    public class MappingTest : Profile
+    {
+        //CreateMap<LibreriaMaterial, LibroMaterialDto>();
+        public MappingTest()
+        {
+            CreateMap<LibreriaMaterial, LibroMaterialDto>();
+        }
+    }
+}

e95d67c2c25e36f50ae0395e62baeed302053be6
facundomarcos <facundomarcos@live.com.ar>
2024-05-09 12:28:10 -0300

    configuracion ocelot

--- TiendaServicios.Api.Gateway/Program.cs
+++ TiendaServicios.Api.Gateway/Program.cs
@@ -6,16 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //builder.Services.AddControllers();
+//builder.Services.AddOcelot();
+//builder.Configuration
+//  .AddJsonFile("ocelot.json");
+// .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json");
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddOcelot();
-builder.Configuration
-    .AddJsonFile("ocelot.json");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
-await app.UseOcelot();
+//await app.UseOcelot();
+app.UseOcelot().Wait();
 app.MapControllers();
 
 app.Run();

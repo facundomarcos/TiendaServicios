f828becaff4e0e226e4317df6500e8b2abb5fd30
facundomarcos <facundomarcos@live.com.ar>
2024-05-10 11:14:21 -0300

    configuracion ocelot y todos los endpoints funcionando ok

--- TiendaServicios.Api.Gateway/Program.cs
+++ TiendaServicios.Api.Gateway/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json");
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();

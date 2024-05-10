47dc8a620b9a1a0efae31eae940cf6999759d749
facundomarcos <facundomarcos@live.com.ar>
2024-05-10 12:15:39 -0300

    Messaje Handler

--- TiendaServicios.Api.Gateway/Program.cs
+++ TiendaServicios.Api.Gateway/Program.cs
@@ -1,5 +1,6 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using TiendaServicios.Api.Gateway.MessajeHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddDelegatingHandler<LibroHandler>();
 
 var app = builder.Build();
 

8773a6147793847982936aaba019a5cfc5f02421
facundomarcos <facundomarcos@live.com.ar>
2024-05-13 10:29:30 -0300

    rabbitmq

--- TiendaServicios.RabbitMQ3.Bus/BusRabbit/IEventoManejador.cs
+++ TiendaServicios.RabbitMQ3.Bus/BusRabbit/IEventoManejador.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
 using TiendaServicios.RabbitMQ.Bus.Eventos;
 
 namespace TiendaServicios.RabbitMQ.Bus.BusRabbit
